5bced7afa15d0bc26e60e1cd6191e687a8369a8d
Quinz15 <167899106+Quinz15@users.noreply.github.com>
2024-06-01 12:59:53 -0300

    Add files via upload

---
+++ trilha-net-api-desafio/Controllers/TarefaController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
+
+namespace TrilhaApiDesafio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefaController : ControllerBase
+    {
+        private readonly OrganizadorContext _context;
+
+        public TarefaController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var tarefa = _context.Tarefas.Find(id);
+
+            if (tarefa == null)
+                return NotFound();
+
+            return Ok(tarefa);
+        }
+
+        [HttpGet("ObterTodos")]
+        public IActionResult ObterTodos()
+        {
+            var tarefas = _context.Tarefas.ToList();
+            return Ok(tarefas);
+        }
+
+        [HttpGet("ObterPorTitulo")]
+        public IActionResult ObterPorTitulo(string titulo)
+        {
+            var tarefas = _context.Tarefas
+                                .Where(x => x.Titulo.Contains(titulo))
+                                .ToList();
+
+            return Ok(tarefas);
+        }
+
+        [HttpGet("ObterPorData")]
+        public IActionResult ObterPorData(DateTime data)
+        {
+            var tarefas = _context.Tarefas
+                                .Where(x => x.Data.Date == data.Date)
+                                .ToList();
+
+            return Ok(tarefas);
+        }
+
+        [HttpGet("ObterPorStatus")]
+        public IActionResult ObterPo
